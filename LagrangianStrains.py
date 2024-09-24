ad21de27ac91d73d2e40886548a4798acdf5918d
ElleStark <elle.stark5645@gmail.com>
2024-09-24 16:38:30 -0600

    working plots with dispersion regime fits

--- LagrangianStrains.py
+++ LagrangianStrains.py
@@ -98,6 +98,12 @@ def add_newfield_to_particles(particle_data, field2=None):
 def main():
     
     ########## PART 1: Extract particle tracking data and add strain and acceleration data, if needed
+    # Load viscous dissipation rate matrix and compute y-averaged epsilon as a function of x
+    # dissipation_matrix = np.load('D:/singlesource_2d_extended/viscous_dissipation_extendedSim.npy')
+    # print(dissipation_matrix.shape)
+    # epsilon_1d = np.mean(dissipation_matrix[100:1101, :], axis=0)
+    # plt.plot(epsilon_1d)
+    # plt.show()
 
     # Load particle tracking data; original dimensions (time, features, particles) = (9001, 3, 180000)
     particle_matrix = np.load('ignore/ParticleTrackingData/particleTracking_sim_extended_n20_fullsim_D1.5_nanUpstream_0to180s_normal.npy')
@@ -279,7 +285,8 @@ def main():
     # D_LL_array = np.zeros((n_tsteps, 20))
     r2_0 = np.zeros((n_tsteps, 20))
     # Select times for plotting - for log-log, always want to have 0 as well as first possible time
-    t_list = np.array([0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.4, 0.6, 0.8, 1, 1.5, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6])
+    # t_list = np.array([0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.4, 0.6, 0.8, 1, 1.5, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6, 6.5])
+    t_list = np.array([0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.4, 0.6, 0.8, 1, 1.5, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5])
     t_list_10 = t_list*10
     # t_list = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
     dt = 0.02
@@ -287,11 +294,12 @@ def main():
     r2_list = np.zeros(len(t_list))
     # r_list = np.zeros(len(t_list))
 
-    # d_release_list = [0, 10, 25, 50]
+    # d_release_list = [4]
     d_release_list = [0, 1, 2, 5, 10, 20, 30, 40, 50]
-    # d_release_list = [0, 1, 2, 3, 4, 5]
+    # d_release_list = [0, 1, 2, 10, 30, 50]
     all_r2_list = np.zeros((len(d_release_list), len(r2_list)))
     all_r0_list = np.zeros((len(d_release_list)))
+    n_particles_list = np.zeros((len(d_release_list)))
     # all_r_list = np.zeros((len(d_release_list)))
     # all_D_LL_list = np.zeros((len(d_release_list)))
     all_idx = 0
@@ -299,6 +307,7 @@ def main():
     for delta_release in d_release_list:
         idx = 0
         batchelor_t = ((delta_release*dt * mean_u)**2)
+        n_particles = n_tsteps * 20
         for compute_t in t_list_idx:
             t=0
             for t in range(n_tsteps-1):
@@ -333,21 +342,27 @@ def main():
             else:
                 r2_0 = r2_0
 
+            n_particles = np.min([n_particles, np.count_nonzero(~np.isnan(r2_array))])
             r2_list[idx] = np.nanmean((r2_array - r2_0)**2)
             # r2_list[idx] = np.nanmean(r2_array)**2
             # r_list[idx] = np.nanmean(r2_array)
             idx += 1
         all_r2_list[all_idx, :] = r2_list
         all_r0_list[all_idx] = np.nanmean(r2_0**2)
+        n_particles_list[all_idx] = n_particles
         # all_r_list[all_idx] = np.nanmean(r_list)
         # all_D_LL_list[all_idx] = np.nanmean(D_LL_array)
         all_idx += 1
+
+
+    print(n_particles_list)
 
     C2 = 2.13
     # epsilon = np.load('ignore/inputs/viscous_dissipation_extendedSim.npy')  # 1201 x 1501 matrix of vals
     # epsilon = 0.001015  # m^2/s^3, for simplicity start with domain average
     # epsilon = 0.0025
     epsilon = 0.006728  # m^2/s^3, first 0.05 m
+    epsilon_end = 0.000165  # m^2/s^3, last 0.05 m
     nu = 1.5 * 10**(-5)  # kinematic viscosity, m/s
     t_eta = 0.1216  # s, kolmogorov microscale in time, entire domain
     g = 0.5  # Richardson constant
@@ -357,7 +372,7 @@ def main():
     # t2_array = t_list_10 ** 2
     # t3_array =t_list_10 ** 3
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 6))
 
     # for i in range(len(d_release_list)-1):
     #     i+=1
@@ -374,32 +389,42 @@ def main():
     # plt.ylabel('<r^2> compensated per Ouellette et al.')
     # plt.title('log-log scaling: initial separations 0 to 1 s')
 
-    for i in range(len(d_release_list)-1):
+    c_list = cmr.take_cmap_colors('cmr.sapphire', int(len(d_release_list)+6))
+
+    for i in range(len(d_release_list)):
         # i+=1
-        plt.loglog(t_list[1:], all_r2_list[i, 1:], label=f'p_sep: {round(d_release_list[i]*dt, 2)}')
+        plt.loglog(t_list[1:], all_r2_list[i, 1:], label=f'{round(d_release_list[i]*dt, 2)}', color = c_list[i+6])
         # plt.loglog(t_list[:], g*epsilon*t_list[:]**(3) + 2*all_r0_list[i]**2 - 2*all_r_list[i]*all_r0_list[i], color='blue')
         # plt.loglog(t_list[:], g*epsilon*t_list[:]**(3) + 2*all_r0_list[i]**2, color='red')
     
     # Exponential fit at early times for particle separations for pairs with 0 initial separation 
-    plt.loglog(t_list[1:8], 0.4*(all_r0_list[0]*np.exp(t_list[1:8]/0.07) - all_r0_list[0]), color='red')
+    plt.loglog(t_list[1:8], 0.4*(all_r0_list[0]*np.exp(t_list[1:8]/0.07) - all_r0_list[0]), 'r--', linewidth=2)
     
     # t^3 fit for later times for particle separations for pairs with 0 initial separation, compensated by t^3
     # plt.loglog(t_list[1:], all_r2_list[0, 1:])
     # plt.scatter(t_list[1], g*epsilon* t_list[1]**3)
     # plt.scatter(t_list[-1], g*0.0001*t_list[-1]**3)
 
-    # t^(3/2) curve for later times
-    plt.loglog(t_list[4:], g*epsilon*t_list[4:]**(3/2), color='black')
+    # t^3 curve for later times
+    # plt.loglog(t_list[4:], g*epsilon*t_list[4:]**(3/2), color='black')
+    epsilon_list = 0.007*t_list[1:]**(-3/2)
+    plt.loglog(t_list[4:], g*epsilon_list[3:]*t_list[4:]**3, '--', color='#80328c', linewidth=2)
     
-    # Expected fit:
-    plt.loglog(t_list[1:8], epsilon*t_list[1:8]**(2), color='black')
+    # # Expected fit for early times but large separations:
+    plt.loglog(t_list[1:8], 0.007*t_list[1:8]**(2), 'k--', linewidth=2)
     # Better fit for our data:
     # plt.loglog(t_list[1:8], 0.8*epsilon*t_list[1:8]**(29/16), color='black')
+
+    # Expected linear regime at end???
+    # plt.loglog(t_list[-9:], 5*epsilon_list[-9:]*t_list[-9:]**(3/2), color='green')
+    
+
     plt.xlabel('t (s)')
     plt.ylabel('<|r-r0|>^2')
     plt.title('log-log scaling: initial separations 0 to 1 s')
 
     plt.legend()
+    plt.savefig('ignore/plots/pairseps_woverlay_0to1_loglog.png', dpi=300)
     plt.show()
 
     # ####### plotting and analyzing strain & acceleration along Lagrangian trajectories ########
